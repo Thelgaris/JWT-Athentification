3a28c49a50e4c80704936a4258a09612b54a4922
Thelgaris <alex.gabarron84@gmail.com>
2022-05-20 15:19:23 +0000

    trying

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
-import "../../styles/home.css";
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { Context } from "../store/appContext";
 
 export const Signup = () => {
+  const { store, actions } = useContext(Context);
   const [user, setUser] = useState({});
 
   const createUser = async () => {
     const response = await fetch(
-      "https://3001-thelgaris-jwtathentific-zpl9m4yaiwv.ws-eu45.gitpod.io/api/signup",
+      "https://3001-thelgaris-jwtathentific-xarmb4m2g0h.ws-eu45.gitpod.io/api/signup",
       {
         method: "POST",
         headers: { "content-type": "application/json" },
@@ -18,24 +21,40 @@ export const Signup = () => {
   };
 
   return (
-    <div className="text-center mt-5">
-      <input
-        onChange={(e) => {
-          setUser({ ...user, email: e.target.value });
-        }}
-      ></input>
-      <input
-        onChange={(e) => {
-          setUser({ ...user, password: e.target.value });
-        }}
-      ></input>
-      <button
-        onClick={() => {
-          createUser();
-        }}
-      >
-        Create User
-      </button>
+    <div className="container">
+      <div className="row mx-auto">
+        <div className="">
+          <input
+            id="email"
+            type="text"
+            className="form-control"
+            placeholder="Email"
+            onChange={(e) => {
+              setUser({ ...user, email: e.target.value });
+            }}
+          />
+          <input
+            id="password"
+            type="text"
+            className="form-control"
+            placeholder="Password"
+            onChange={(e) => {
+              setUser({ ...user, password: e.target.value });
+            }}
+          />
+        </div>
+        <div className="d-grid gap-2  mx-auto">
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => {
+              createUser();
+            }}
+          >
+            Registrar
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
